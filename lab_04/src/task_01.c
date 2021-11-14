57ccfd2071e14f563ae4ee619ddb4611aaf7ec7b
Dmitry Varin <varindv@gmail.com>
2021-11-15 01:36:17 +0300

    start lab_04

---
+++ lab_04/src/task_01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+    const int n = 2;
+    int child_processes[n];
+
+    printf("PARENT BEFORE FORK -- PID: %d GROUP PID: %d\n", getpid(), getpgrp());
+
+    for (int i = 0; i < n; i++) {
+        child_processes[i] = fork();
+
+        if (child_processes[i] == -1) {
+            printf("Can't fork\n");
+            return EXIT_FAILURE;
+        }
+
+        if (child_processes[i] == 0) {
+            sleep(2);
+            printf("CHILD %d -- PID: %d PARENT PID: %d GROUP PID: %d\n", i + 1, \
+                    getpid(), getppid(), getpgrp());
+
+            return EXIT_SUCCESS;
+        }
+    }
+
+    printf("PARENT AFTER FORK -- PID: %d GROUP PID: %d CHILDS 1: %d CHILD 2: %d\n", \
+            getpid(), getpgrp(), child_processes[0], child_processes[1]);
+
+    return EXIT_SUCCESS;
+}
