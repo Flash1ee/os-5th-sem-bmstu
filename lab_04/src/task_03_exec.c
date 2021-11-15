c2d132062259ccf4ec6f67be8bcd28168d879e91
Dmitry Varin <varindv@gmail.com>
2021-11-15 11:05:14 +0300

    task_3 complete

---
+++ lab_04/src/task_03_exec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("IN EXEC PROC\n");
+    printf("Hello, world!\n");
+    printf("I am a task that is executed using exec\n");
+
+    return 0;
+}
